31b77cff1ef7720d0d8e587420b5ce77605a28de
leechangwoo <jedd0331@ezcaretech.com>
2023-11-07 08:40:45 +0900

    Message 조회

--- webadmin/src/com/mirth/connect/webadmin/utils/Constants.java
+++ webadmin/src/com/mirth/connect/webadmin/utils/Constants.java
@@ -12,6 +12,8 @@ package com.mirth.connect.webadmin.utils;
 public class Constants {
     public static final String DASHBOARD_STATS = "/DashboardStatistics.action";
     public static final String DASHBOARD_STATS_JSP = "/WEB-INF/jsp/dashboardstatistics.jsp";
+    public static final String SEARCH_MESSAGE = "/SearchMessage.action";
+    public static final String SEARCH_MESSAGE_JSP = "/WEB-INF/jsp/searchmessage.jsp";
     public static final String INDEX_PAGE = "/Index.action";
     public static final String INDEX_JSP = "/WEB-INF/jsp/index.jsp";
 }
